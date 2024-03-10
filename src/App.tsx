efc903f48b3a82101684b8e5a9c0dc76de2bb764
Jake Dobler <jaecob.dobler@reusser.com>
2024-03-10 14:48:38 -0400

    added animation to hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import "./App.css";
 import ImageGrid from "./components/ImageGrid";
 import main from "/photos/DM004914.jpg";
+import { motion } from "framer-motion";
 
 function App() {
+  const loadingVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  const textVariants = {
+    hidden: {
+      opacity: 0,
+      y: 40,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
   return (
     <div className="h-screen">
-      <nav className=" h-screen relative">
-        <div className="z-10 absolute left-0 right-0 top-0 bottom-5 flex flex-col items-center justify-end text-white">
+      <motion.nav
+        variants={loadingVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 3 }}
+        className=" h-screen relative"
+      >
+        <motion.div
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 5 }}
+          className="z-10 absolute left-0 right-0 top-0 bottom-5 flex flex-col items-center justify-end text-white background-slate-900/20"
+        >
           <h1 className="font-bold text-4xl lg:text-6xl z-10">
             Jordan & Justice
           </h1>
           <strong className="text-6xl lg:text-8xl"> Gilliam</strong>
           <h2 className="text-2xl lg:text-4xl z-10">Feburary 24th 2024</h2>
-          <a href="#img-grid">View</a>
-        </div>
+          <a
+            href="#img-grid"
+            className="hover:border-slate-300 hover:text-slate-300 border-2 border-white rounded-lg px-4 my-2"
+          >
+            View
+          </a>
+        </motion.div>
         <img
           src={main}
           alt="Jordan & Justice Gilliam"
           className="absolute top-0 z-0 object-cover h-full w-full"
         />
-      </nav>
+      </motion.nav>
       <div id="img-grid" className="pt-4">
         <ImageGrid />
       </div>
